9932da4601dcaa7bbeb6113e3af08d7eec309a90
SAIKRISHNA <korutlasaikrishna25@gmail.com>
2022-12-29 00:16:34 +0530

    hi

---
+++ src/main/java/com/kmit1/kmit1/App.java
@@ -0,0 +1,13 @@
+package com.kmit1.kmit1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
